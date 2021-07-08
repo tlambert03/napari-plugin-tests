41e89407aec91b4cae2930fe6a5bbbf889f574f3
Talley Lambert <talley.lambert@gmail.com>
2021-07-08 15:12:03 -0400

    add requirements

--- setup.py
+++ setup.py
@@ -1,2 +1,4 @@
 # tox requires this to find files in the current dir
-__import__("setuptools").setup()
+__import__("setuptools").setup(
+    name="napari-plugin-tests", install_requires=["pydantic", "PyYAML", "tox"]
+)
